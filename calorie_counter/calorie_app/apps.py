4ae6afd38d908a154c049d14d0797ba945d6431c
Suciu Andrei <100287042+21Suciu@users.noreply.github.com>
2025-02-21 18:40:30 +0200

    Delete calorie_counter directory

--- calorie_counter/calorie_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class CalorieAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'calorie_app'
--- calorie_counter/calorie_app/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-
-
-class FoodLogForm(forms.Form):
-    food_name = forms.CharField(max_length=255)
-    calories = forms.FloatField()
-    portion_size = forms.CharField(max_length=100, required=False)
-
